de6cfa69b097c46e02082feda76dc17908178c43
Gaetano Ruggiero <Gaetano Ruggiero@LAPTOP-87QNT9GN>
2019-09-16 10:47:28 +0200

    user bean

---
+++ src/model/UserBean.java
@@ -0,0 +1,53 @@
+package logineser;
+
+public class UserBean {
+
+	public UserBean() {
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String nome;
+	private String cognome;
+	private int telefono;
+	private String username;
+	private String password;
+	private String email;
+	
+
+}
